8d53ff24ce91d59fa6cf40c6adecc3a5a5cacef4
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-13 12:13:05 +1300

    center

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,12 +1,28 @@
 import request from 'superagent'
 
+
 export function getGreeting() {
   return request.get('/greeting').then((res) => res.body.greeting)
 }
 
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6089442d10msh761447d6ab1388cp1bf39ejsn9308ea310980',
+		'X-RapidAPI-Host': 'tennisapi1.p.rapidapi.com'
+	}
+};
+
 export function getTennisData() {
-  return request
-    .get('https://rapidapi.com/BettingAPI/api/tennis-odds/details')
-    .then((res) => res.body.activity)
+  return fetch('https://tennisapi1.p.rapidapi.com/api/tennis/search/nadal', options)
+    .then((res) => res.json())
+    .catch((err) => console.error(err)) // I would personally handle this error in the component, so that we can show the user something useful if the fetch fails, like, "Hey User! I couldn't fetch this for you."
 }
+
+
+// export function getTennisData() {
+//   return request
+//     .get('https://rapidapi.com/BettingAPI/api/tennis-odds/details')
+//     .then((res) => res.body.activity)
+// }
