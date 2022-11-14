c4eac8b3dbee9051a43c4fd43ea24fca38925ed7
tom mcivor <ttmcivor@gmail.com>
2022-11-15 11:56:03 +1300

    add

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function Post({ title, date }) {
+
+ let thisdate = new Date(date * 1000).toLocaleString()
+
   return (
     <>
   <div>{title}</div>
-  <div >{date}</div>
+  <div >{thisdate}</div>
   </>
   )
 }
--- client/components/SubredditList.jsx
+++ client/components/SubredditList.jsx
@@ -3,13 +3,17 @@ import { useSelector } from 'react-redux'
 
 import Post from './Post'
 
+
+
 function Subreddit() {
   const subreddits = useSelector((state) => state.subreddits)
   console.log(subreddits[0]);
+
+
   return (
     <div>
       {subreddits.map((post, i) => (
-        <Post key={i} title={post.title} />
+        <Post key={i} title={post.title} date={post.created} />
       ))}
     </div>
   )
