2c7d320fc69575e6e2ba9a844107a3e24fa8127c
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-21 00:48:25 +1300

    add

--- client/components/Tennis.jsx
+++ client/components/Tennis.jsx
@@ -5,11 +5,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-import {  useDispatch , useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchPlayer } from '../actions'
 
-import { fetchTennisPlayer , fetchttennispplayer  } from '../apis/tennis'
+import { fetchTennisPlayer, fetchttennispplayer } from '../apis/tennis'
 
 function Tennis(props) {
 
@@ -32,41 +32,49 @@ function Tennis(props) {
 
   const handleSubmit = async (evt) => {
     evt.preventDefault()
-    dispatch(fetchPlayer(formData.name))
+
     let player = await fetchTennisPlayer(formData.name)
 
+    dispatch(fetchPlayer(formData.name))
     props.loadedPlayer(player)
 
+    setFormData({
+      name: '',
+    })
     console.log(player)
   }
 
   return (
-    <div>
+    <>
+
+      <div>
 
 
-      <Box
-        onSubmit={handleSubmit}
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="PlayerName:" variant="outlined"
-          name="name"
-          onChange={handleChange}
-          value={formData.name}
+        <Box
+          onSubmit={handleSubmit}
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
         >
-        </TextField>
+          <TextField id="outlined-basic" label="PlayerName:" variant="outlined"
+            name="name"
+            onChange={handleChange}
+            value={formData.name}
+          >
+          </TextField>
 
-        <Button type="submit" variant="contained">
-          View Player
-        </Button>
-      </Box>
-
-      {Object.keys(tennis).length != 0 && tennis.results[0].entity.id}
-    </div>
+          <Button type="submit" variant="contained">
+            View Player
+          </Button>
+        </Box>
+      </div>
+      <div>
+        {Object.keys(tennis).length != 0 && tennis.results[0].entity.id}
+      </div>
+    </>
   )
 
 }
